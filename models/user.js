2b2b01b7c377bf66f5214efce80fd96d6540753d
sahil patel <98449155+sahilpatel9910@users.noreply.github.com>
2024-05-16 20:08:54 +1000

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return User;
+};
